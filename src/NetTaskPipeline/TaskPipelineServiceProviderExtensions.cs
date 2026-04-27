8c545f414faaf1c5ab5901546f4173034f2b64b1
heber mattos <feanorkun@hotmail.com>
2026-04-27 20:56:41 -0300

    Remove explicit service provider overloads

--- src/NetTaskPipeline/TaskPipelineServiceProviderExtensions.cs
+++ src/NetTaskPipeline/TaskPipelineServiceProviderExtensions.cs
@@ -1,5 +1,4 @@
 using System;
-using Microsoft.Extensions.DependencyInjection;
 
 namespace NetTaskPipeline;
 
@@ -16,50 +15,5 @@ public static class TaskPipelineServiceProviderExtensions
             throw new ArgumentNullException(nameof(pipeline));
 
         return pipeline.RegisterServiceProvider(serviceProvider);
-    }
-
-    public static TaskPipeline AddTask<TTask>(
-        this TaskPipeline pipeline,
-        IServiceProvider serviceProvider,
-        int? retryCount = null,
-        TimeSpan? timeout = null,
-        string? name = null)
-        where TTask : ITask
-    {
-        if (pipeline == null)
-            throw new ArgumentNullException(nameof(pipeline));
-
-        if (serviceProvider == null)
-            throw new ArgumentNullException(nameof(serviceProvider));
-
-        return pipeline.AddTask(
-            ActivatorUtilities.GetServiceOrCreateInstance<TTask>(serviceProvider),
-            retryCount,
-            timeout,
-            name ?? typeof(TTask).Name);
-    }
-
-    public static TaskPipeline AddParallel<TTask1, TTask2>(
-        this TaskPipeline pipeline,
-        IServiceProvider serviceProvider,
-        int? retryCount = null,
-        TimeSpan? timeout = null)
-        where TTask1 : ITask
-        where TTask2 : ITask
-    {
-        if (pipeline == null)
-            throw new ArgumentNullException(nameof(pipeline));
-
-        if (serviceProvider == null)
-            throw new ArgumentNullException(nameof(serviceProvider));
-
-        return pipeline.AddParallel(
-            new ITask[]
-            {
-                ActivatorUtilities.GetServiceOrCreateInstance<TTask1>(serviceProvider),
-                ActivatorUtilities.GetServiceOrCreateInstance<TTask2>(serviceProvider)
-            },
-            retryCount,
-            timeout);
     }
 }

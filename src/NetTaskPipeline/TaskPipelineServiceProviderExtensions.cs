c509b49d39ed87b115d32d1896c84c56748a62ca
heber mattos <feanorkun@hotmail.com>
2026-04-27 20:51:31 -0300

    Add WithServiceProvider extension

--- src/NetTaskPipeline/TaskPipelineServiceProviderExtensions.cs
+++ src/NetTaskPipeline/TaskPipelineServiceProviderExtensions.cs
@@ -5,6 +5,19 @@ namespace NetTaskPipeline;
 
 public static class TaskPipelineServiceProviderExtensions
 {
+    /// <summary>
+    /// Registers the service provider used by generic task registration methods.
+    /// </summary>
+    public static TaskPipeline WithServiceProvider(
+        this TaskPipeline pipeline,
+        IServiceProvider serviceProvider)
+    {
+        if (pipeline == null)
+            throw new ArgumentNullException(nameof(pipeline));
+
+        return pipeline.RegisterServiceProvider(serviceProvider);
+    }
+
     public static TaskPipeline AddTask<TTask>(
         this TaskPipeline pipeline,
         IServiceProvider serviceProvider,
